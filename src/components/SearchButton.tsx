e536b98505f3a12de79356fed4bb8ebeb7591ed3
Majesty-jun <hkwns01@gmail.com>
2023-05-04 19:32:34 +0900

    [fix] import 경로 수정

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ReactComponent as SearchLogo } from '../../assets/search.svg';
+import { ReactComponent as SearchLogo } from '../assets/search.svg';
 
 export const SearchButton = () => {
   return (
--- src/components/SearchList.tsx
+++ src/components/SearchList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IData } from '../../../types/types';
+import { IData } from '../types/types';
 import styled from 'styled-components';
 
 interface Props {
